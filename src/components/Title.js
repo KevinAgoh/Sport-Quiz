8a77910bbcdcce6143849091fda78ec9db31a4b8
Kevin Agoh <kevinagoh@hotmail.fr>
2021-08-23 16:07:45 +0200

    first commit

---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Title() {
+  return (
+    <div>
+      <div className="title">
+        <h1>Welcome to Kéum's sport quiz</h1>
+      </div>
+    </div>
+  )
+}
