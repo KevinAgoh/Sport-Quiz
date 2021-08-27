d4bf316978a7f894d2abe5df2c112efe2d11d7f4
Kevin Agoh <kevinagoh@hotmail.fr>
2021-08-27 09:32:26 +0200

    set up the grid differently, one player at the time now

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,21 +4,27 @@ import './card.css';
 
 export default function Card(props) {
 
-  const [showInfos, setShowInfos] = useState(false);
+  const [showInfos, setShowInfos] = useState(false)
 
   return (
-    <div className="player-card">
+    <div className="player-card mt-5">
       <img src={props.picture} alt="" /> 
       <div className="player-details">
         {showInfos &&
-        <>
-          <h3>{props.name}</h3>
-          <p>{props.team}</p>
-          <p>{props.height}</p>
-          <p>{props.position}</p>
-        </>
+          <>
+            <h3>{props.name}</h3>
+            <p>{props.team}</p>
+            <p>{props.height}</p>
+            <p>{props.position}</p>
+          </>
         }
-        <button onClick={() => setShowInfos(!showInfos)}>Show player name</button>
+        <button 
+          onClick={() => {
+            setShowInfos(!showInfos);
+          }}
+          type="button" 
+          className="btn btn-primary"
+        >Show player name</button>
       </div>
     </div>
   );
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from './Card';
 import './grid.css';
 import {players} from './database.js';
 
 export default function Grid() {
+
+  const [currentPlayer, setCurrentPlayer] = useState(0);
+
+  const handleNextPlayer = () => {
+    setCurrentPlayer(currentPlayer + 1);
+  };
+
   return (
-    <div className="parent">
-      {players.map(player =>(
-        <Card
-          picture={player.picture}
-          name={player.name}
-          team={player.team}
-          height={player.height}
-          position={player.position}
-        />
-      ))}
-    </div>
+    <>
+      <div className="parent">
+          <Card
+            picture={players[currentPlayer].picture}
+            name={players[currentPlayer].name}
+            team={players[currentPlayer].team}
+            height={players[currentPlayer].height}
+            position={players[currentPlayer].position}
+          />
+      </div>
+      <div className="next-player d-flex justify-content-center">
+        <button className="next-player-button mt-5" onClick={handleNextPlayer}>Next player</button>
+      </div>
+    </>
   )
 }
