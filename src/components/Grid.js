c3c676f9513c3003d79bc1b15b954478c32a8d96
Kevin Agoh <kevinagoh@hotmail.fr>
2021-09-02 16:40:27 +0200

    added AOS to the card

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,8 +1,12 @@
 import React, {useState} from 'react';
 import {players} from './database.js';
 import './grid.scss';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function Grid() {
+
+  AOS.init();
 
   const [currentPlayer, setCurrentPlayer] = useState(0);
   let [showInfos, setShowInfos] = useState(false);
@@ -45,37 +49,40 @@ export default function Grid() {
     <>
       <h3>Try to guess the player's name 😀</h3>
       <div className="parent">
-        <div className="player-card mt-5">
-          <img src={players[currentPlayer].picture} alt="" /> 
-          <div className="player-details">
-            {showInfos &&
-              <>
-                <h3>{players[currentPlayer].name}</h3>
-                <p>{players[currentPlayer].team}</p>
-                <p>{players[currentPlayer].height}</p>
-                <p>{players[currentPlayer].position}</p>
-              </>
-            }
+        <div data-aos="fade-in">
+          <div className="player-card mt-5">
+            <img src={players[currentPlayer].picture} alt="" /> 
+            <div className="player-details">
+              {showInfos &&
+                <>
+                  <h3>{players[currentPlayer].name}</h3>
+                  <p>{players[currentPlayer].team}</p>
+                  <p>{players[currentPlayer].height}</p>
+                  <p>{players[currentPlayer].position}</p>
+                </>
+              }
+            </div>
+            <button 
+              onClick={() => {
+                handleShowInfo();
+              }}
+              type="button"
+              className="disclose-button">
+              Show player name
+            </button>
           </div>
-          <button 
-            onClick={() => {
-              handleShowInfo();
-            }}
-            type="button"
-            className="disclose-button">
-            Show player name
-          </button>
+          <div className="next-player d-flex justify-content-center">
+            <button 
+              className="next-player-button mt-5" 
+              onClick={() => {
+                handleNextPlayer();
+                playAudio();
+                AOS.init();
+              }}>
+                Next player
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="next-player d-flex justify-content-center">
-        <button 
-          className="next-player-button mt-5" 
-          onClick={() => {
-            handleNextPlayer();
-            playAudio();
-          }}>
-            Next player
-        </button>
       </div>
     </>
   )
